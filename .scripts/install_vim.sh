2578c4671f75167daeb85852bf76c8a40950b83d
Lucas McDaniel <lmcdaniel@myemma.com>
2018-04-30 09:06:37 -0500

    Updated install and update scripts and submodules

--- .scripts/install_vim.sh
+++ .scripts/install_vim.sh
@@ -1,33 +1,55 @@
 #!/bin/bash
 
-brew install macvim cmake;
+brew install macvim cmake vim --with-override-system-vi;
 brew update;
+pip install -U pip;
 
 while true; do
-  read -p "Do you want to use the .vimrc in this repo (y/n)? " ack
-  case $ack in
+  read -p "Do you want to use the .vimrc in this repo (y/n)? " answer
+  case $answer in
     [Yy]* ) echo "Good choice!"; break;;
     [Nn]* ) echo "Okay ;)"; break;;
     * ) echo "Please answer yes or no.";;
   esac
 done
 while true; do
-  read -p "Is it okay to remove your .vimrc and replace it with the one in this repo (y/n)? " nd
-  case $nd in
-    [Yy]* ) rm ~/.vimrc; ln -s ~/.vim/.vimrc ~/.vimrc;
+  read -p "Is it okay to remove your .vimrc and replace it with the one in this repo (y/n)? " vimrc
+  case $vimrc in
+    [Yy]* ) unlink ~/.vimrc; rm ~/.vimrc; ln -s ~/.vim/.vimrc ~/.vimrc;
+      break;;
+    [Nn]* ) echo "Okay ;)"; break;;
+    * ) echo "Please answer yes or no.";;
+  esac
+done
+while true; do
+  read -p "Do you want to initialize YouCompleteMe for autocompletion?" YouCompleteMe
+  case $YouCompleteMe in
+    [Yy]* ) cd ~/.vim/plug/plugins/start/YouCompleteMe;
+      git submodule update --init --recursive;
+      ./install.py --all;
+      cd ~/.vim;
+      break;;
+    [Nn]* ) echo "Okay ;)"; break;;
+    * ) echo "Please answer yes or no.";;
+  esac
+done
+while true; do
+  read -p "Do you want to install sass-lint?" sass
+  case $sass in
+    [Yy]* ) npm install -g sass-lint
+      break;;
+    [Nn]* ) echo "Okay ;)"; break;;
+    * ) echo "Please answer yes or no.";;
+  esac
+done
+while true; do
+  read -p "Do you want to install tidy-html5 hmtl linter?" html5
+  case $html5 in
+    [Yy]* ) brew install tidy-html5;
       break;;
     [Nn]* ) echo "Okay ;)"; break;;
     * ) echo "Please answer yes or no.";;
   esac
 done
 
-cd ~/dotfiles/.vim/;
-git submodule update --init --recursive;
-~/.vim/pack/plugins/start/YouCompleteMe/install.py;
-pip install -U pip;
-pip install --upgrade pyflakes;
-pip install pycodestyle;
-npm install -g pug-lint;
-npm install -g jshint;
-npm install -g sass-lint
-brew install tidy-html5;
+
--- .scripts/update.sh
+++ .scripts/update.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-git submodule update --remote --merge;
+while true; do
+  read -p "Do you want to update the submodules in this project?" subs
+  case $subs in
+    [Yy]* ) git submodule update --remote --merge;
+      break;;
+    [Nn]* ) echo "Updating ;)"; break;;
+    * ) echo "Please answer yes or no.";;
+  esac
+done
